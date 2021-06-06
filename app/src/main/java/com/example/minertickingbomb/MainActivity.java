b68addc71e68dbb8a345b5473cabf23ee7447d2d
merkel1860 <merkelmller@yandex.ru>
2021-06-06 01:11:12 -0400

    Almost done.

--- app/src/main/java/com/example/minertickingbomb/MainActivity.java
+++ app/src/main/java/com/example/minertickingbomb/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.widget.LinearLayoutCompat.LayoutParams;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,13 +14,24 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+import java.util.UUID;
+
+import static com.example.minertickingbomb.R.string.start_btn;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    LinearLayoutCompat parentContainer;
-    Stack<Miner> miners = new Stack<>();
-    Button startButton, restartButton, cancelButton ;
+    private LinearLayoutCompat parentContainer;
+    private Stack<Miner> miners = new Stack<>();
+    private Button startButton, restartButton;
+    private Button cancelButton ;
+    /**
+     *
+     */
+    private final int STARTBUTTONID = (int) UUID.randomUUID().getMostSignificantBits();
+    private final int CANCELBUTTONID = (int) UUID.randomUUID().getMostSignificantBits();
 
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,12 +62,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startButton = new Button(this);
         startButton.setText("Start");
         startButton.setOnClickListener(this);
-//        startButton.setId(R.string.start_btn);
+        startButton.setId(STARTBUTTONID);
 
         cancelButton = new Button(this);
         cancelButton.setText("Cancel");
         cancelButton.setOnClickListener(this);
-//        cancelButton.setId(R.id.cancel_btn);
+        cancelButton.setId(CANCELBUTTONID);
 
         LayoutParams systemButtonParams = makeLayoutParams(50,2,
                 2,0,0);
@@ -89,14 +99,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         LayoutParams buttonLayoutParams = makeLayoutParams(25,2,2,
                 0,0);
         for (int i=0; i<columns;i++){
-            Miner button = new Miner(context);
-            button.setText("Hello");
-            button.setId(((int) button.getIdentifierMiner().getMostSignificantBits()));
+            Miner mine = new Miner(context);
+            mine.setText("Hello");
+            mine.setValue(pseudoRandomGenerator());
+            mine.setId(((int) mine.getIdentifierMiner().getMostSignificantBits()));
             // TODO checking Miner id consistency
             //            System.out.println(button.getIdentifierMiner()+"/"+button.getId());
-            button.setOnClickListener(this);
-            miners.push(button);
-            linearLayoutCompatRow.addView(button,buttonLayoutParams);
+            mine.setOnClickListener(this);
+            miners.push(mine);
+            linearLayoutCompatRow.addView(mine,buttonLayoutParams);
         }
     }
 
@@ -119,8 +130,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         System.out.println("Height : "+height + " width :"+width);
     }
 
+    private int pseudoRandomGenerator(){
+        return (int)(Math.random()*1000);
+    }
     @Override
     public void onClick(View v) {
 
+        if(v.getId() == STARTBUTTONID){
+            Toast.makeText(this,"You got: " + STARTBUTTONID,Toast.LENGTH_SHORT).show();
+
+        }else if(v.getId() == CANCELBUTTONID){
+            Toast.makeText(this,"You got: " + CANCELBUTTONID,Toast.LENGTH_SHORT).show();
+        }else {
+            // TODO review received value in getValue, it eeems that there is some inconsistency
+            Toast.makeText(this,"You got: " + ((Miner)findViewById(v.getId())).getValue(),Toast.LENGTH_SHORT).show();
+        }
     }
 }
